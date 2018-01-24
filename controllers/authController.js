814203ba4a28448dd7b3d70d9aaeadcd94935a74
stephmen <stephmen@gmail.com>
2018-01-24 13:44:24 -0500

    Password Validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -38,8 +38,30 @@ user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
 user.resetPasswordExpires = Date.now() + 3600000; // one hour from now
 await user.save();
 // 3. send an email with token
-const resetURL =`http://${req.headers.host}.account/reset/${user.resetPasswordToken});`
+const resetURL =`http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
 req.flash('Success', `You have been emailed a password reset link. ${resetURL}`)
 // 4. redirect to login page
 res.redirect('/login'); 
+};
+
+exports.reset = async (req, res) => {
+  const user = await User.findOne({
+    resetPasswordToken: req.params.token,
+    resetPasswordExpires: { $gt: Date.now() }
+  });
+  if (!user) {
+    req.flash('error', 'Password reset is invalid or has expired');
+    return res.redirect('/login');
+  }
+  // if there is a user, show the rest of password form
+  res.render('reset', {title: 'Reset your Password'});    
+};
+
+exports.confirmedPasswords = (req, res, next) => {
+  if (req.body.password === req.body['password-confirm']){
+    next(); //Keep Going! 
+    return;
+  }
+req.flash('error', 'Password do not match!')
+res.redirect('back')
 };
